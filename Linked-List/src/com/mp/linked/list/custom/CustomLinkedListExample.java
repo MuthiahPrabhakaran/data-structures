529237bdab8b75773d2ee87e7a1381556912ce25
Muthiah Prabhakaran <muthiahprabhakaran@gmail.com>
2020-06-11 10:13:08 +0530

    implemented - indexOf, contains, removeFirst methods

--- Linked-List/src/com/mp/linked/list/custom/CustomLinkedListExample.java
+++ Linked-List/src/com/mp/linked/list/custom/CustomLinkedListExample.java
@@ -3,8 +3,18 @@ package com.mp.linked.list.custom;
 public class CustomLinkedListExample {
     public static void main(String[] args) {
         var list = new LinkedList();
-        list.addFirst(10);
-        list.addFirst(20);
-        list.addFirst(30);
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addFirst(5);
+
+//        System.out.println(list.indexOf(10));
+//        System.out.println(list.indexOf(200));
+
+//        System.out.println(list.contains(10));
+//        System.out.println(list.contains(100));
+
+        list.removeFirst();
+        System.out.println(list.contains(5));
     }
 }
--- Linked-List/src/com/mp/linked/list/custom/LinkedList.java
+++ Linked-List/src/com/mp/linked/list/custom/LinkedList.java
@@ -1,5 +1,7 @@
 package com.mp.linked.list.custom;
 
+import java.util.NoSuchElementException;
+
 public class LinkedList {
 
     private class Node {
@@ -11,16 +13,61 @@ public class LinkedList {
         }
     }
 
-    Node first; //head
-    Node last; //tail
+    Node first; // head
+    Node last; // tail
 
-    public void addFirst(int item){
+    public void addLast(int item){
         var node = new Node(item);
-        if(first == null)
+        if(isEmpty())
             first = last = node;
         else {
             last.next = node;
             last = node;
         }
+    }
+
+    public void addFirst(int item){
+        var node = new Node(item);
+        if(isEmpty())
+            first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
+    }
+
+    public int indexOf(int item) {
+        var current = first;
+        int index = 0;
+        while(current != null){
+            if(current.value == item)
+                return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int item){
+        return indexOf(item) != -1;
+    }
+
+    public void removeFirst(){
+        // If the list is empty
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        // If the list has only one node
+        if(first == last){
+            first = last = null;
+            return;
+        }
+
+        var second = first.next;
+        first = second;
+    }
+
+    private boolean isEmpty(){
+        return first == null;
     }
 }
